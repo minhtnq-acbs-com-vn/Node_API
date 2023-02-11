37087a15363d9c23ec24cfed4567a68b02ce459d
kou1153 <truongminh453534@gmail.com>
2023-02-11 21:55:18 +0700

    Format response to all objects

--- controllers/mobile.js
+++ controllers/mobile.js
@@ -4,7 +4,7 @@ import { dbRead } from "../utils/databaseManage.js";
 const getMobile = asyncHandler(async (req, res, next) => {
   let documents = await dbRead("Mobile");
   if (documents.length < 1) throw new Error(`Collection lost default document`);
-  return res.status(200).json(documents);
+  return res.status(200).json({documents});
 });
 
 export { getMobile };
