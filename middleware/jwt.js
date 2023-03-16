b7045f1189071de54fd17e3af6ca1009e95b2e1a
Quang Minh <71371833+kou1153@users.noreply.github.com>
2023-03-16 20:03:33 +0700

    fix key again

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,15 +1,17 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (payload, expiredTime) => {
-  return jwt.sign(payload, process.env.apiKey, {
+  return jwt.sign(payload, `${process.env.apiKey}`, {
     expiresIn: expiredTime,
   });
 };
 
 const validateToken = (req, res, next) => {
   const authHeader = req.headers["auth"];
+  console.log(authHeader);
   if (authHeader === undefined) throw new Error(`Invalid token`);
-  jwt.verify(authHeader, process.env.apiKey, (err, payload) => {
+  jwt.verify(authHeader, `${process.env.apiKey}`, (err, payload) => {
+    console.log(`${process.env.apiKey}`);
     if (err !== null) throw new Error(`Invalid token`);
     next();
   });
