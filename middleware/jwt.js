e9d1307eebcfed868cc53884cfcc3fc463965ca8
Quang Minh <71371833+kou1153@users.noreply.github.com>
2023-03-16 17:29:06 +0700

    trigger ci/cd

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -5,7 +5,6 @@ const generateToken = (payload, expiredTime) => {
     expiresIn: expiredTime,
   });
 };
-
 const validateToken = (req, res, next) => {
   const authHeader = req.headers["auth"];
   if (authHeader === undefined) throw new Error(`Invalid token`);
