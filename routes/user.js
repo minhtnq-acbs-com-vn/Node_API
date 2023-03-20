b8cdd2a8a3d6285f991dc6076333bba0c9d1b671
Quang Minh <71371833+kou1153@users.noreply.github.com>
2023-03-20 15:14:32 +0700

    fix path for import

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { validateToken } from "./middleware/jwt.js";
+import { validateToken } from "../middleware/jwt.js";
 import { login, passwordChange } from "../controllers/user.js";
 const router = express.Router();
 
