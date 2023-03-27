4a8060b249fbc0c3ded6e62deed3aba3402fc8e5
Quang Minh <71371833+kou1153@users.noreply.github.com>
2023-03-27 23:43:51 +0700

    fix test push noti i hope

--- controllers/firebase.js
+++ controllers/firebase.js
@@ -1,15 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { initializeApp, cert } from "firebase-admin/app";
-import { getMessaging } from "@firebase/messaging";
+import { getMessaging } from "firebase-admin/messaging";
 
 const pushNoti = asyncHandler(async (req, res, next) => {
-  const app = initializeApp({
+  initializeApp({
     credential: cert(JSON.parse(process.env.firebaseKey)),
     databaseURL:
       "https://iot-data-22125-default-rtdb.asia-southeast1.firebasedatabase.app",
   });
   const registrationToken =
-    "ep_Bo_46T40sk3QvLzchvk:APA91bFU58JUFx1cmXJxOrtUFGgXZ2CEyKWmEACdOKYJYPXvXzXqysoHRm3AMtsZE819gHZXGmgP3_ikpRflqDddOLCMtyrSrK9MashCtIA71 GilkaiW3MpZVBWVEK8JQjDxkoG41OMI";
+    "eE67QA0LSk2bGEb86pGuxv:APA91bEGGDXBS_ieUhwmYvT5iGrH5MYFGarSxccS6xlQuQQIX8VcmJbfbUuR0sw_ZVaEZtlyKcn8ghoPp1uAGyfxTn4Mxv8TS87BDWDCbZHQlJ92otZYb9gw95oV7anReSg_QT3TYX-k";
 
   const message = {
     data: {
@@ -21,9 +21,7 @@ const pushNoti = asyncHandler(async (req, res, next) => {
 
   // Send a message to the device corresponding to the provided
   // registration token.
-
-  let { response, error } = await getMessaging(app).send(message);
-  console.log("error: ", error);
+  let response = await getMessaging().send(message);
   console.log("response: ", response);
 
   res.status(200).json({ success: true });
